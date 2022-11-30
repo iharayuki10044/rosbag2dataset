98796688c0e79e89e020f9d92740bb2408c7dbb5
iharayuki10044 <iharayuki0103@gmail.com>
2022-11-30 16:21:59 +0900

    update

--- utils.py
+++ utils.py
@@ -47,10 +47,10 @@ def convert_MonoCompressedImage_Resized(data, height=None, width=None):
     for msg in tqdm(data):
         img = cv2.imdecode(np.fromstring(msg.data, np.uint8), cv2.IMREAD_GRAYSCALE)
         if height is not None and width is not None:
-            h,w,c = img.shape
+            # h,w,c = img.shape
             #img = img[0:h, int((w-h)*0.5):w-int((w-h)*0.5), :]
             img = cv2.resize(img, (height, width))
-        obs.append(img)
+        obsd.append(img)
     return obsd
 
 def convert_Odometry(data, action_noise, lower_bound, upper_bound):
